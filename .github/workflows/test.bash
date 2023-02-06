61a56c6af988268ab4f6bd2c575366cbb75712b5
Mostafa Farzan <m2_farzan@yahoo.com>
2023-02-06 10:24:26 +0330

    Remove sip workaround from the tests

--- .github/workflows/test.bash
+++ .github/workflows/test.bash
@@ -7,10 +7,6 @@ chown -R ${AUR_USER}:${AUR_USER} .
 
 sudo pacman -Syyu --noconfirm --noprogressbar
 
-# Temporary fix (https://github.com/m2-farzan/ros2-galactic-PKGBUILD/issues/9)
-pacman -S wget python-pip python-pyqt5 --noconfirm --noprogressbar
-pip3 install pyqt5==5.15.5
-
 cat .SRCINFO | grep -oP "depends\ \=\ \K.+" | xargs sudo -u ${AUR_USER} yay -S --noconfirm --noprogressbar --needed
 
 sudo -u ${AUR_USER} makepkg
